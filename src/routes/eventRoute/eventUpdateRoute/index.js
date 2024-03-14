30b2bdbc6c9e60f54d9284c224cc47ddaea991bd
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2024-03-14 13:05:23 +0600

    add: event update route

---
+++ src/routes/eventRoute/eventUpdateRoute/index.js
@@ -0,0 +1,15 @@
+import expres, { query } from "express"
+import EventCollection from "../../../models/Event.js"
+
+var router = expres.Router()
+router.put("/event/:id", async(req, res)=>{
+    const getId = req.params.id
+    const data = req.body
+    const query = {_id:getId}
+    const result = await EventCollection.findOneAndUpdate(query, {
+        $set : data
+    })
+    res.send(result)
+})
+
+export default router
