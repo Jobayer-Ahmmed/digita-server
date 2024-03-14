968cc32013a6f59f8d693ea390d45be4141ff71e
Jobayer-Ahmmed <jobayer.ahmmed.jas@gmail.com>
2024-03-14 12:57:57 +0600

    add: event post route

---
+++ src/routes/eventRoute/eventPostRoute/index.js
@@ -0,0 +1,14 @@
+import express from "express"
+import EventCollection from "../../../models/Event.js"
+
+
+
+const router = express.Router()
+
+router.post("/event", async(req, res)=>{
+    const data = req.body
+    const sentData = new EventCollection(data)
+    const result = await sentData.save()
+    res.send(result)
+})
+export default router
